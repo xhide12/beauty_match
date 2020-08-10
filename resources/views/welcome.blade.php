bedcb424dd6983a4714e4a04a491a93c270b5355
Hiroshi Nakano <f.c.pyro@gmail.com>
2020-08-10 20:52:18 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelはじめます
+                    Laravel
                 </div>
 
                 <div class="links">
