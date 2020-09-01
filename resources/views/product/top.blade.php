ae1ec2c480fe6b03368836172599d851b43af65a
xhide <f.c.pyro@gmail.com>
2020-09-01 23:24:50 +0900

    商品一覧画面のレイアウト変更

--- resources/views/product/top.blade.php
+++ resources/views/product/top.blade.php
@@ -2,8 +2,11 @@
 
 @section('product_index')
     商品一覧<br>
-    <table>
+    <table class="table"
+        <thead>
         <tr><th>ブランド名</th><th>商品名</th><th>商品カテゴリー</th><th>サイズ</th><th>メーカー名</th><th>画像１</th><th>画像２</th><th>画像３</th><th>画像４</th><th>商品説明</th><th>成分・原材料</th><th>オフィシャルHP</th><th>オフィシャルInstagram</th></tr>
+        </thead>
+        <tbody>
     @foreach($products as $product)
         <tr>
             <td>{{ $product->brand }}</td>
@@ -11,10 +14,10 @@
             <td>{{ $product->category }}</td>
             <td>{{ $product->size }}</td>
             <td>{{ $product->manufacture }}</td>
-            <td><img src="{{ asset('/storage/'.$product->image1)}}"></td>
-            <td><img src="{{ asset('/storage/'.$product->image2)}}"></td>
-            <td><img src="{{ asset('/storage/'.$product->image3)}}"></td>
-            <td><img src="{{ asset('/storage/'.$product->image4)}}"></td>
+            <td><img src="{{ asset('/storage/'.$product->image1)}}" class="img-thumbnail"></td>
+            <td><img src="{{ asset('/storage/'.$product->image2)}}" class="img-thumbnail"></td>
+            <td><img src="{{ asset('/storage/'.$product->image3)}}" class="img-thumbnail"></td>
+            <td><img src="{{ asset('/storage/'.$product->image4)}}" class="img-thumbnail"></td>
             <td>{{ $product->product_coment }}</td>
             <td>{{ $product->composition }}</td>
             <td>{{ $product->official_hp }}</td>
@@ -28,6 +31,7 @@
 
         </tr>
     @endforeach
+        </tbody>
     </table>
 
 @endsection
