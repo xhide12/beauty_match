fca67bc91cdfa659b99206ff8022f3355165b79e
xhide <f.c.pyro@gmail.com>
2020-09-21 22:28:01 +0900

    topページの修正

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -12,28 +12,71 @@
   <div class="carousel-inner" role="listbox">
     <div class="carousel-item active">
         <img src="image/top-image-1.png" alt="responsive image" class="d-block img-fluid">
-        <div class="carousel-caption d-none d-md-block text-muted font-weight-bold">
-          <h1>beautymatch</h1>
-          <p>美容系フリーランスと化粧品メーカーをつなぐマッチングサイト</p>
+        <div class="carousel-caption d-none d-md-block text-dark  font-weight-bold">
+        <h5>”人”と"感情"と"信頼"の時代の</h5>
+        <h5>美容系商品のサンプリングプラットフォーム</h5><br><br><br><br><br><br>
         </div>
     </div>
     <div class="carousel-item">
         <img src="image/top-image-2.png" alt="responsive image" class="d-block img-fluid">
-        <div class="carousel-caption d-none d-md-block text-light font-weight-bold">
-          <h1>beautymatch</h1>
-          <p>美容系フリーランスと化粧品メーカーをつなぐマッチングサイト</p>
+        <div class="carousel-caption d-none d-md-block text-Light font-weight-bold">
+          <h5>”人”と"感情"と"信頼"の時代の</h5>
+          <h5>美容系商品のサンプリングプラットフォーム</h5><br><br><br><br><br><br>
         </div>
 
     </div>
     <div class="carousel-item">
         <img src="image/top-image-3.png" alt="responsive image" class="d-block img-fluid">
-        <div class="carousel-caption d-none d-md-block text-light font-weight-bold">
-          <h1>beautymatch</h1>
-          <p>美容系フリーランスと化粧品メーカーをつなぐマッチングサイト</p>
+        <div class="carousel-caption d-none d-md-block text-Light font-weight-bold">
+        <h5>”人”と"感情"と"信頼"の時代の</h5>
+          <h5>美容系商品のサンプリングプラットフォーム</h5><br><br><br><br><br><br>
         </div>
 
     </div>
   </div>
+</div>
+
+<br><br>
+<div class="card-deck">
+  <div class="d-flex flex-row">
+    <div class="card mb-6" style="max-width: 600px">
+      <div class="row no-gutters">
+            <div class="col-md-4 my-auto">
+                <img class="card-img" src="image/user-icon-1.png">
+            </div>
+            <div class="col-md-8 text-center">
+                <div class="card-body">
+                <h4 class="card-title">for Beauty Freelance</h4>
+                <p class="card-text">お客様に商品紹介したい美容系フリーランスの方<br>(インフルエンサーになりたい方)</p>
+                @unless (Auth::guard('user')->check())
+                  <a href="{{ route('user.register') }}" class="btn btn-primary">新規登録</a>　<a href="{{ route('user.login') }}" class="btn btn-primary">ログイン</a>
+                @else
+                  <a href="{{ route('user.home') }}" class="btn btn-primary">マイページ</a>
+                @endunless
+                </div>
+            </div>
+      </div>
+    </div>
+    <div class="card mb-6" style="max-width: 600px">
+      <div class="row no-gutters">
+        <div class="col-md-4 my-auto">
+            <img class="card-img" src="image/manufacture-icon-1.png">
+        </div>
+        <div class="col-md-8 text-center">
+            <div class="card-body">
+            <h4 class="card-title">for COMPANY</h4>
+            <p class="card-text">商品PRやプロモーションを依頼したい方<br>(インフルエンサーになりたい方)</p>
+            @unless (Auth::guard('manufacture')->check())
+              <a href="{{ route('manufacture.register') }}" class="btn btn-primary">新規登録</a>　<a href="{{ route('manufacture.login') }}" class="btn btn-primary">ログイン</a>
+            @else   
+              <a href="{{ route('manufacture.home') }}" class="btn btn-primary">管理画面</a>
+            @endunless
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </div>
 
 <br><br>
@@ -50,9 +93,13 @@
 
 <br><br>
 
-<div class="card-deck">
+<div class="font-weight-bold scrollanime downup d-flex justify-content-between">
+<h3>紹介してほしい商品</h3>
+<h6><a href="{{ route('product_index') }}">もっと見る＞＞</a></h6>
+</div>
+<div class="card-deck card-group row-cols-1 row-cols-md-3">
     @foreach($products as $product)
-    <div class="card col-xs-4 scrollanime downup">
+    <div class="card col-xs-4 scrollanime downup" style="width: 18rem">
       <img class="bd-placeholder-img card-img-top" width="100%" height="350" src="{{ asset('/storage/'.$product->image1)}}" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/></img>
       <div class="card-body bg-light">
         <h5 class="card-title font-weight-bold">{{ $product->brand }}</h5>
