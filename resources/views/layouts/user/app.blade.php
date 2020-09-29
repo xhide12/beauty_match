ba4a645a1b223b63e70bc504ed14bcf0f01cac82
xhide <f.c.pyro@gmail.com>
2020-09-29 23:21:10 +0900

    バナーの追加

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -97,6 +97,10 @@
        list-style: none;
     }
 
+    .product_bannar{
+        padding-bottom:50px
+    }
+
     </style>
 
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
