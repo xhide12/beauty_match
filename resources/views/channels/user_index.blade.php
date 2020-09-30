31b8817e519ae1cb8b9bcfcd24fb637014289a4e
Hiroshi Nakano Mac <xhide@dd.mbn.or.jp>
2020-10-01 01:52:09 +0900

    no message

--- resources/views/channels/user_index.blade.php
+++ resources/views/channels/user_index.blade.php
@@ -13,16 +13,13 @@
                         </ul>
                     </div>
 
-                    <div class="card-body">
-                        <div class="row">
-                            <textarea type="text" id="text" cols="80" rows="2"></textarea>
-                            <input type="hidden" id="introduction_id" value="{{ $introduction->id }}">
-                            <input type="hidden" id="user_id" value="{{ $introduction->user_id }}">
-                            <input type="hidden" id="manufacture_id" value="{{ $introduction->manufacture_id }}">
-                            <input type="submit" class="btn btn-primary" value="送信" id="submit">
-                        </div>
-                        <br><br>
-                        <a type="submit" class="btn btn-secondary" href="{{ route('user.home') }}">マイページ</a>
+                    <div class="card-body bg-dark">
+                        <input type="text" id="text">
+                        <input type="hidden" id="user_id" value="{{ $introduction->user_id }}">
+                        <input type="hidden" id="manufacture_id" value="{{ $introduction->manufacture_id }}">
+                        <input type="submit" value="送信" id="submit"><br><br>
+
+                        <a type="submit" class="btn btn-primary" href="{{ route('user.home') }}">マイページ</a>
                     </div>
                 </div>
             </div>
